794002ea2d74020041caf59a6a7403c224236dd5
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-02-20 12:20:30 -0500

    bumped version number

--- energy_pandas/__init__.py
+++ energy_pandas/__init__.py
@@ -5,7 +5,7 @@ from outdated import warn_if_outdated
 from .energypandas import EnergySeries, EnergyDataFrame
 
 # Version of the package
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 # warn if a newer version of archetypal is available
 warn_if_outdated("energy-pandas", __version__)
