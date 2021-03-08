96782049c088cb188c4f2a53c101a0cba339ec15
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-03-08 17:26:31 -0500

    fixes package version at runtime

--- energy_pandas/__init__.py
+++ energy_pandas/__init__.py
@@ -3,9 +3,14 @@
 from outdated import warn_if_outdated
 
 from .energypandas import EnergySeries, EnergyDataFrame
+from pkg_resources import get_distribution, DistributionNotFound
 
 # Version of the package
-__version__ = "0.1.0"
+try:
+    __version__ = get_distribution("archetypal").version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "0.0.0"  # should happen only if package is copied, not installed.
 
 # warn if a newer version of archetypal is available
 warn_if_outdated("energy-pandas", __version__)
